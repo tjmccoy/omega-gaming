74b535312c39f7c49d909bbab48e0e6c5e76d93d
tjmccoy <tyjmccoy@outlook.com>
2026-02-09 14:24:01 -0600

    add joinLottery

--- Contracts/lotteryImpl.sol
+++ Contracts/lotteryImpl.sol
@@ -5,10 +5,13 @@ pragma solidity ^0.8.30;
 import {Initializable}      from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable}    from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 // CONTRACT
 contract OmegaLottery is Initializable, UUPSUpgradeable, OwnableUpgradeable
 {
+    using Strings for uint256;
+
     // ERRORS
     error InvalidEntryTime();
 
@@ -45,6 +48,7 @@ contract OmegaLottery is Initializable, UUPSUpgradeable, OwnableUpgradeable
     // STORAGE
     uint256 public lotteryIdCounter;    // incrementing lottery ID. starts @ 1
     mapping(uint256 => Lottery) internal lotteries; // lotteryId => Lottery 
+    mapping(uint256 => address[]) internal lotteryPlayers;  // lotteryId => players
     uint256[45] private __gap;  // reserved storage gap for future updates
 
     
@@ -78,6 +82,17 @@ contract OmegaLottery is Initializable, UUPSUpgradeable, OwnableUpgradeable
             startTime,
             endTime
         );
+    }
+
+    // JOIN LOTTERY
+    function joinLottery(uint256 lotteryId) external payable
+    {
+        Lottery storage lottery = lotteries[lotteryId];
+
+        require(msg.value >=  lottery.entryFee, string.concat("Entry fee (eth): ", lottery.entryFee.toString()));
+        require(lottery.status == LotteryStatus.OPEN, "Lottery is not yet joinable.");
+
+        lotteryPlayers[lotteryId].push(msg.sender);
     }
 
     // VIEW FUNCTIONS (for debugging/development)
