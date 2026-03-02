e041a8438f2a43e86d028567195a201a2de3e12d
Preston <Psailor454@yahoo.com>
2026-03-02 13:45:50 -0600

    Changed timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,25 +69,6 @@ function App() {
   const connectors = useConnectors()
   const { disconnect } = useDisconnect()
 
-    const [timeRemaining, setTimeRemaining] = useState('')
-  
-  useEffect(() => {
-    const countDownDate = new Date("Feb 25, 2026 15:00:00").getTime();
-
-    const x = setInterval(function() {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      setTimeRemaining(` ${hours}h ${minutes}m ${seconds}s`);
-    }, 1000);
-
-    return () => clearInterval(x);
-  }, []);
-
   return (
 <>
     <WagmiProvider config={config}>
